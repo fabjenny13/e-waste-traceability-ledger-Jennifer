63f227101a79a0151cf4f85bcafda0b03ec71735
Jennifer <jenn10fav@gmail.com>
2026-09-20 00:20:17 +0530

    auth refactored

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const prisma = require("./utils/prisma");
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/authRoutes");
 const deviceRoutes = require("./routes/deviceRoutes");
 
 const app = express();
